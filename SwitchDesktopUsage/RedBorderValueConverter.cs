695ce115691d0a64d045cae44f98cc5fd149fa9c
lanxiaofang <13604461032@163.com>
2022-09-20 00:31:17 +0800

    first commit

---
+++ SwitchDesktopUsage/RedBorderValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SwitchDesktopUsage
+{
+    public class RedBorderValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                return -doubleValue;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
